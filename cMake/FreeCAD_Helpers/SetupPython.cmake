60403b16f3e86c3a82c3c91160d858f89baeddfc
Joao Matos <joao@tritao.eu>
2024-12-29 23:11:05 +0000

    CMake: Check for and generate a Python home dir config value.

--- cMake/FreeCAD_Helpers/SetupPython.cmake
+++ cMake/FreeCAD_Helpers/SetupPython.cmake
@@ -7,4 +7,12 @@ macro(SetupPython)
          message(FATAL_ERROR "To build FreeCAD you need at least Python 3.10\n")
     endif()
 
+    # If a custom Python directory was passed in, then save it as PYTHON_HOME_DIR,
+    # which is used by config.h.cmake when generating the config header file.
+    if (DEFINED Python3_ROOT_DIR)
+        set(HAVE_PYTHON_HOME_DIR ON)
+        file(REAL_PATH "${Python3_ROOT_DIR}" PYTHON_HOME_DIR)
+    endif ()
+
+
 endmacro(SetupPython)
