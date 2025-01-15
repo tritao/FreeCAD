14d30350ef31f32a1a7c2f9e2b082c857eddf333
tritao <joao@tritao.eu>
2025-01-15 17:48:40 +0000

    Gui: Fix rendering for overriden display modes in different views.

--- src/Gui/View3DInventorViewer.h
+++ src/Gui/View3DInventorViewer.h
@@ -460,6 +460,7 @@ public:
 
 protected:
     static GLenum getInternalTextureFormat();
+    void preRenderUpdate();
     void renderScene();
     void renderFramebuffer();
     void renderGLImage();
