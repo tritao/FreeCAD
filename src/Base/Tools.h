31e2de3d9557d28d0a2cad254e6549c9e90b0b4d
tritao <joao@tritao.eu>
2026-01-05 15:05:41 +0000

    base: remove QString overloads from Base::Tools

--- src/Base/Tools.h
+++ src/Base/Tools.h
@@ -45,9 +45,6 @@ class connection;
 }
 }  // namespace boost
 
-
-class QString;
-
 namespace Base
 {
 
@@ -361,10 +358,7 @@ struct BaseExport Tools
     static std::string escapedUnicodeFromUtf8(const char* s);
     static std::string escapedUnicodeToUtf8(const std::string& s);
     static std::string escapeQuotesFromString(const std::string& s);
-
-    static QString escapeEncodeString(const QString& s);
     static std::string escapeEncodeString(const std::string& s);
-    static QString escapeEncodeFilename(const QString& s);
     static std::string escapeEncodeFilename(const std::string& s);
 
     /**
