503d6cfaac2323c86d08007f9407bb8e8ecb9d9f
tritao <joao@tritao.eu>
2026-01-13 12:06:20 +0000

    Remove Boost version header

--- src/Gui/VerboseInfoQt.h
+++ src/Gui/VerboseInfoQt.h
@@ -8,8 +8,6 @@
 #include <map>
 #include <string>
 
-#include <boost/version.hpp>
-
 #include <QDir>
 #include <QFile>
 #include <QFileInfo>
@@ -45,8 +43,7 @@ inline QString prettyProductInfoWrapper()
 {
     auto productName = QSysInfo::prettyProductName();
 #ifdef FC_OS_MACOSX
-    auto macosVersionFile =
-        QStringLiteral("/System/Library/CoreServices/.SystemVersionPlatform.plist");
+    auto macosVersionFile = QStringLiteral("/System/Library/CoreServices/.SystemVersionPlatform.plist");
     auto fi = QFileInfo(macosVersionFile);
     if (fi.exists() && fi.isReadable()) {
         auto plistFile = QFile(macosVersionFile);
@@ -70,7 +67,8 @@ inline QString prettyProductInfoWrapper()
 #ifdef FC_OS_WIN64
     QSettings regKey {
         QStringLiteral("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"),
-        QSettings::NativeFormat};
+        QSettings::NativeFormat
+    };
     if (regKey.contains(QStringLiteral("CurrentBuildNumber"))) {
         auto buildNumber = regKey.value(QStringLiteral("CurrentBuildNumber")).toInt();
         if (buildNumber > 0) {
@@ -113,8 +111,10 @@ inline void addModuleInfo(QTextStream& str, const QString& modPath, bool& firstM
         }
     }
     catch (const Base::Exception& e) {
-        auto what = QString::fromUtf8(e.what()).trimmed().replace(QChar::fromLatin1('\n'),
-                                                                  QChar::fromLatin1(' '));
+        auto what = QString::fromUtf8(e.what()).trimmed().replace(
+            QChar::fromLatin1('\n'),
+            QChar::fromLatin1(' ')
+        );
         str << " (Malformed metadata: " << what << ")";
     }
     QFileInfo disablingFile(mod.absoluteFilePath(), QStringLiteral("ADDON_DISABLED"));
@@ -127,12 +127,14 @@ inline void addModuleInfo(QTextStream& str, const QString& modPath, bool& firstM
 
 inline void getVerboseCommonInfo(QTextStream& str, const std::map<std::string, std::string>& mConfig)
 {
-    const QString deskEnv =
-        QProcessEnvironment::systemEnvironment().value(QStringLiteral("XDG_CURRENT_DESKTOP"),
-                                                       QString());
-    const QString deskSess =
-        QProcessEnvironment::systemEnvironment().value(QStringLiteral("DESKTOP_SESSION"),
-                                                       QString());
+    const QString deskEnv = QProcessEnvironment::systemEnvironment().value(
+        QStringLiteral("XDG_CURRENT_DESKTOP"),
+        QString()
+    );
+    const QString deskSess = QProcessEnvironment::systemEnvironment().value(
+        QStringLiteral("DESKTOP_SESSION"),
+        QString()
+    );
 
     const QString major = getValueOrEmpty(mConfig, "BuildVersionMajor");
     const QString minor = getValueOrEmpty(mConfig, "BuildVersionMinor");
@@ -153,9 +155,10 @@ inline void getVerboseCommonInfo(QTextStream& str, const std::map<std::string, s
 
     const QString sysType = QSysInfo::productType();
     if (sysType != QLatin1String("windows") && sysType != QLatin1String("macos")) {
-        QString sessionType =
-            QProcessEnvironment::systemEnvironment().value(QStringLiteral("XDG_SESSION_TYPE"),
-                                                          QString());
+        QString sessionType = QProcessEnvironment::systemEnvironment().value(
+            QStringLiteral("XDG_SESSION_TYPE"),
+            QString()
+        );
         if (sessionType == QLatin1String("x11")) {
             sessionType = QStringLiteral("xcb");
         }
@@ -214,7 +217,7 @@ inline void getVerboseCommonInfo(QTextStream& str, const std::map<std::string, s
     str << "Qt " << QT_VERSION_STR << ", ";
     str << "Coin " << fcCoin3dVersion << ", ";
     str << "Vtk " << fcVtkVersion << ", ";
-    str << "boost " << BOOST_LIB_VERSION << ", ";
+    str << "boost " << fcBoostVersion << ", ";
     str << "Eigen3 " << fcEigen3Version << ", ";
     str << "PySide " << fcPysideVersion << '\n';
     str << "shiboken " << fcShibokenVersion << ", ";
@@ -231,8 +234,7 @@ inline void getVerboseCommonInfo(QTextStream& str, const std::map<std::string, s
         ifcopenshellVer = Base::Interpreter().getValue(cmd, "version");
     }
     catch (const Base::Exception& e) {
-        Base::Console().log(
-            "%s (safe to ignore, unless using the BIM workbench and IFC).\n", e.what());
+        Base::Console().log("%s (safe to ignore, unless using the BIM workbench and IFC).\n", e.what());
     }
 
     if (ifcopenshellVer) {
@@ -246,9 +248,9 @@ inline void getVerboseCommonInfo(QTextStream& str, const std::map<std::string, s
 
 #if defined(HAVE_OCC_VERSION)
     str << "OCC " << OCC_VERSION_MAJOR << "." << OCC_VERSION_MINOR << "." << OCC_VERSION_MAINTENANCE
-#ifdef OCC_VERSION_DEVELOPMENT
+# ifdef OCC_VERSION_DEVELOPMENT
         << "." OCC_VERSION_DEVELOPMENT
-#endif
+# endif
         << '\n';
 #endif
     QLocale loc;
@@ -271,23 +273,25 @@ inline void getVerboseCommonInfo(QTextStream& str, const std::map<std::string, s
     }
     str << "\n";
 
-    ParameterGrp::handle hGrp =
-        App::GetApplication().GetParameterGroupByPath("User parameter:BaseApp/Preferences/View");
+    ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath(
+        "User parameter:BaseApp/Preferences/View"
+    );
 
-    QString navStyle =
-        QString::fromStdString(hGrp->GetASCII("NavigationStyle", "Gui::CADNavigationStyle"));
+    QString navStyle = QString::fromStdString(
+        hGrp->GetASCII("NavigationStyle", "Gui::CADNavigationStyle")
+    );
     // All navigation styles are named on the format "Gui::<Name>NavigationStyle"
     // so we remove the "Gui::" prefix and the "NavigationStyle" suffix before printing.
     navStyle.replace(QRegularExpression(QLatin1String("^Gui::")), {});
     navStyle.replace(QRegularExpression(QLatin1String("NavigationStyle$")), {});
 
-    const QString orbitStyle =
-        QStringLiteral("Turntable,Trackball,Free Turntable,Trackball Classic,Rounded Arcball")
-            .split(QLatin1Char(','))
-            .at(hGrp->GetInt("OrbitStyle", 4));
-    const QString rotMode =
-        QStringLiteral("Window center,Drag at cursor,Object center").split(QLatin1Char(',')).at(
-            hGrp->GetInt("RotationMode", 0));
+    const QString orbitStyle
+        = QStringLiteral("Turntable,Trackball,Free Turntable,Trackball Classic,Rounded Arcball")
+              .split(QLatin1Char(','))
+              .at(hGrp->GetInt("OrbitStyle", 4));
+    const QString rotMode = QStringLiteral("Window center,Drag at cursor,Object center")
+                                .split(QLatin1Char(','))
+                                .at(hGrp->GetInt("RotationMode", 0));
 
     str << QStringLiteral("Navigation Style/Orbit Style/Rotation Mode: %1/%2/%3\n")
                .arg(navStyle, orbitStyle, rotMode);
