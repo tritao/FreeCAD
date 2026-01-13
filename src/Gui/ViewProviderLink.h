fc63d1451bb1164633057f7e60766d3cbd0ef7c7
tritao <joao@tritao.eu>
2026-01-13 15:23:23 +0000

    Remove Boost.IntrusivePtr

--- src/Gui/ViewProviderLink.h
+++ src/Gui/ViewProviderLink.h
@@ -29,6 +29,8 @@
 #include <vector>
 #include <memory>
 
+#include <Base/IntrusivePtr.h>
+
 #include "Selection/SoFCUnifiedSelection.h"
 #include "ViewProviderDocumentObject.h"
 #include "ViewProviderExtension.h"
@@ -44,7 +46,7 @@ namespace Gui
 {
 
 class LinkInfo;
-using LinkInfoPtr = boost::intrusive_ptr<LinkInfo>;
+using LinkInfoPtr = Base::IntrusivePtr<LinkInfo>;
 
 #if defined(_MSC_VER)
 // forward declaration to please MSVC
