290369832747da3668a9726d053e32b291177bee
tritao <joao@tritao.eu>
2026-01-13 19:24:30 +0000

    Remove Boost.Statechart

--- src/Gui/Navigation/NavigationStateChart.h
+++ src/Gui/Navigation/NavigationStateChart.h
@@ -24,7 +24,8 @@
 
 #pragma once
 
-#include <boost/statechart/event.hpp>
+#include <memory>
+
 #include <Gui/Navigation/NavigationStyle.h>
 
 // NOLINTBEGIN(cppcoreguidelines-avoid*, readability-avoid-const-params-in-decls)
@@ -40,7 +41,7 @@ class GuiExport NavigationStateChart: public UserNavigationStyle
     TYPESYSTEM_HEADER_WITH_OVERRIDE();
 
 public:
-    struct Event: public boost::statechart::event<Event>
+    struct Event
     {
         using Button = SoMouseButtonEvent::Button;
         using Key = SoKeyboardEvent::Key;
@@ -119,30 +120,6 @@ public:
     virtual ~NaviStateMachine() = default;
 
     virtual void process_event(const NavigationStateChart::Event&) = 0;
-};
-
-template<typename T>
-class NaviStateMachineT: public NaviStateMachine
-{
-public:
-    explicit NaviStateMachineT(T* t)
-        : object(t)
-    {
-        object->initiate();
-    }
-
-    ~NaviStateMachineT() override
-    {
-        object.reset();
-    }
-
-    void process_event(const NavigationStateChart::Event& ev) override
-    {
-        object->process_event(ev);
-    }
-
-private:
-    std::unique_ptr<T> object;
 };
 
 }  // namespace Gui
