371cc3f85174400a3e434c5c0569512ec4f11b91
tritao <joao@tritao.eu>
2026-01-13 23:58:47 +0000

    Remove Boost.Preprocessor (CAM)

--- src/Mod/CAM/App/Area.cpp
+++ src/Mod/CAM/App/Area.cpp
@@ -141,8 +141,8 @@ void AreaParams::dump(const char* msg) const
 CAreaConfig::CAreaConfig(const CAreaParams& p, bool noFitArcs)
 {
 #define AREA_CONF_SAVE_AND_APPLY(_param) \
-    PARAM_FNAME(_param) = BOOST_PP_CAT(CArea::get_, PARAM_FARG(_param))(); \
-    BOOST_PP_CAT(CArea::set_, PARAM_FARG(_param))(p.PARAM_FNAME(_param));
+    PARAM_FNAME(_param) = FC_PP_CAT(CArea::get_, PARAM_FARG(_param))(); \
+    FC_PP_CAT(CArea::set_, PARAM_FARG(_param))(p.PARAM_FNAME(_param));
 
     PARAM_FOREACH(AREA_CONF_SAVE_AND_APPLY, AREA_PARAMS_CAREA);
 
@@ -155,8 +155,7 @@ CAreaConfig::CAreaConfig(const CAreaParams& p, bool noFitArcs)
 CAreaConfig::~CAreaConfig()
 {
 
-#define AREA_CONF_RESTORE(_param) \
-    BOOST_PP_CAT(CArea::set_, PARAM_FARG(_param))(PARAM_FNAME(_param));
+#define AREA_CONF_RESTORE(_param) FC_PP_CAT(CArea::set_, PARAM_FARG(_param))(PARAM_FNAME(_param));
 
     PARAM_FOREACH(AREA_CONF_RESTORE, AREA_PARAMS_CAREA);
 }
