134ff9d92c12366c8fb495abc4d6536d4f6676b8
Joao Matos <joao@tritao.eu>
2026-04-08 23:58:26 +0100

    BIM: migrate Space footprint display to generic mode

--- src/Mod/BIM/bimtests/TestArchSpaceGui.py
+++ src/Mod/BIM/bimtests/TestArchSpaceGui.py
@@ -2,7 +2,7 @@
 
 # ***************************************************************************
 # *                                                                         *
-# *   Copyright (c) 2013 Yorik van Havre <yorik@uncreated.net>              *
+# *   Copyright (c) 2026 FreeCAD contributors                               *
 # *                                                                         *
 # *   This file is part of FreeCAD.                                         *
 # *                                                                         *
@@ -22,39 +22,27 @@
 # *                                                                         *
 # ***************************************************************************
 
-"""Import all Arch module unit tests in GUI mode."""
+"""GUI regressions for Arch Space footprint display."""
 
-from bimtests.TestArchImportersGui import TestArchImportersGui
-from bimtests.TestArchAxisGui import TestArchAxisGui
-from bimtests.TestArchBuildingPartGui import TestArchBuildingPartGui
-from bimtests.TestArchFootprintGui import TestArchFootprintGui
-from bimtests.TestArchStairsGui import TestArchStairsGui
-from bimtests.TestArchReportGui import TestArchReportGui
-from bimtests.TestArchSiteGui import TestArchSiteGui
-from bimtests.TestArchSpaceGui import TestArchSpaceGui
-from bimtests.TestArchStructureGui import TestArchStructureGui
-from bimtests.TestArchWallGui import TestArchWallGui
-from bimtests.TestArchWindowGui import TestArchWindowGui
-from bimtests.TestWebGLExportGui import TestWebGLExportGui
-from bimtests.TestArchCoveringGui import TestArchCoveringGui
-
-TEST_CLASSES = (
-    TestArchImportersGui,
-    TestArchAxisGui,
-    TestArchBuildingPartGui,
-    TestArchStairsGui,
-    TestArchReportGui,
-    TestArchSiteGui,
-    TestArchWallGui,
-    TestArchWindowGui,
-    TestWebGLExportGui,
-    TestArchCoveringGui,
-)
+import Arch
+import Part
+from bimtests import TestArchBaseGui
 
 
-def load_tests(loader, _tests, _pattern):
-    """Return the complete Arch GUI test suite explicitly."""
-    suite = loader.suiteClass()
-    for test_class in TEST_CLASSES:
-        suite.addTests(loader.loadTestsFromTestCase(test_class))
-    return suite
+class TestArchSpaceGui(TestArchBaseGui.TestArchBaseGui):
+
+    def test_space_populates_footprint_display_data(self):
+        """Spaces should expose footprint display data through the generic mode."""
+
+        base = self.document.addObject("Part::Feature", "GuiSpaceBox")
+        base.Shape = Part.makeBox(1000, 500, 2000)
+        space = Arch.makeSpace([base])
+        self.document.recompute()
+        self.pump_gui_events()
+
+        proxy = space.ViewObject.Proxy
+        self.assertIn("Footprint", space.ViewObject.listDisplayModes())
+        self.assertTrue(hasattr(proxy, "fcoords"))
+        self.assertTrue(hasattr(proxy, "fset"))
+        self.assertGreater(proxy.fcoords.point.getNum(), 0)
+        self.assertGreater(proxy.fset.coordIndex.getNum(), 0)
