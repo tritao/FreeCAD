a1fb69f92fa559fe33c04f525b667a7a2b3d59eb
Joao Matos <joao@tritao.eu>
2026-04-14 02:28:04 +0100

    Gui: harden InputField locale parsing

--- src/Gui/InputField.cpp
+++ src/Gui/InputField.cpp
@@ -255,37 +255,46 @@ void InputField::contextMenuEvent(QContextMenuEvent* event)
 void InputField::newInput(const QString& text)
 {
     Quantity res;
-    try {
+    QString errorText;
+    auto tryParse = [&](const QString& input) -> bool {
+        try {
+            if (isBound()) {
+                std::shared_ptr<Expression> e(
+                    ExpressionParser::parse(getPath().getDocumentObject(), input.toUtf8())
+                );
+
+                setExpression(e);
+
+                std::unique_ptr<Expression> evalRes(getExpression()->eval());
+
+                auto* value = freecad_cast<NumberExpression*>(evalRes.get());
+                if (value) {
+                    res.setValue(value->getValue());
+                    res.setUnit(value->getUnit());
+                }
+            }
+            else {
+                res = Quantity::parse(input.toStdString());
+            }
+            return true;
+        }
+        catch (Base::Exception& e) {
+            errorText = QString::fromLatin1(e.what());
+            return false;
+        }
+    };
+
+    if (!tryParse(text)) {
         QString input = text;
         fixup(input);
-
-        if (isBound()) {
-            std::shared_ptr<Expression> e(
-                ExpressionParser::parse(getPath().getDocumentObject(), input.toUtf8())
-            );
-
-            setExpression(e);
-
-            std::unique_ptr<Expression> evalRes(getExpression()->eval());
-
-            auto* value = freecad_cast<NumberExpression*>(evalRes.get());
-            if (value) {
-                res.setValue(value->getValue());
-                res.setUnit(value->getUnit());
+        if (input == text || !tryParse(input)) {
+            if (iconLabel->isHidden()) {
+                iconLabel->setVisible(true);
             }
+            Q_EMIT parseError(errorText);
+            validInput = false;
+            return;
         }
-        else {
-            res = Quantity::parse(input.toStdString());
-        }
-    }
-    catch (Base::Exception& e) {
-        QString errorText = QString::fromLatin1(e.what());
-        if (iconLabel->isHidden()) {
-            iconLabel->setVisible(true);
-        }
-        Q_EMIT parseError(errorText);
-        validInput = false;
-        return;
     }
 
     if (res.isDimensionless()) {
@@ -780,25 +789,34 @@ void InputField::fixup(QString& input) const
 QValidator::State InputField::validate(QString& input, int& pos) const
 {
     Q_UNUSED(pos);
-    try {
-        Quantity res;
-        QString text = input;
-        fixup(text);
-        res = Quantity::parse(text.toStdString());
+    Quantity res;
+    auto tryParse = [&](const QString& text) {
+        try {
+            res = Quantity::parse(text.toStdString());
+            return true;
+        }
+        catch (Base::Exception&) {
+            return false;
+        }
+    };
 
-        double factor;
-        std::string unitStr;
-        res.getUserString(factor, unitStr);
-        double value = res.getValue() / factor;
-        // disallow one to enter numbers out of range
-        if (value > this->Maximum || value < this->Minimum) {
-            return QValidator::Invalid;
+    QString text = input;
+    if (!tryParse(text)) {
+        fixup(text);
+        if (text == input || !tryParse(text)) {
+            // Actually invalid input but the newInput slot gives
+            // some feedback
+            return QValidator::Intermediate;
         }
     }
-    catch (Base::Exception&) {
-        // Actually invalid input but the newInput slot gives
-        // some feedback
-        return QValidator::Intermediate;
+
+    double factor;
+    std::string unitStr;
+    res.getUserString(factor, unitStr);
+    double value = res.getValue() / factor;
+    // disallow one to enter numbers out of range
+    if (value > this->Maximum || value < this->Minimum) {
+        return QValidator::Invalid;
     }
 
     return QValidator::Acceptable;
