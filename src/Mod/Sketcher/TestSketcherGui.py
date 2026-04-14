839294c218eb7d8cfb79f9c0ed97f85e9216fd74
Joao Matos <joao@tritao.eu>
2026-04-14 01:50:24 +0100

    Sketcher: move GUI tests to dedicated module

---
+++ src/Mod/Sketcher/TestSketcherGui.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from SketcherTests.TestPlacementUpdate import TestSketchPlacementUpdate
+
+# Use the module so that code checkers don't complain (flake8)
+True if TestSketchPlacementUpdate else False
