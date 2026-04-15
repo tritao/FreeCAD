dfc55ddc3d7fdd76f7e4efb42d3e32389a86c115
Joao Matos <joao@tritao.eu>
2026-04-15 15:53:33 +0100

    BIM: add plan contract helpers for equipment assets

--- src/Mod/BIM/bimtests/TestArchEquipment.py
+++ src/Mod/BIM/bimtests/TestArchEquipment.py
@@ -85,3 +85,28 @@ class TestArchEquipment(TestArchBase.TestArchBase):
         self.assertEqual(1, len(faces))
         self.assertAlmostEqual(900 * 450, faces[0].Area, delta=0.1)
         self.assertAlmostEqual(equip.Shape.BoundBox.ZMin, faces[0].BoundBox.ZMin, delta=0.001)
+
+    def test_equipment_get_footprint_builds_faces_from_closed_plan_symbol_wires(self):
+        box = self.document.addObject("Part::Box", "Box")
+        box.Length = 500
+        box.Width = 2000
+        box.Height = 1200
+
+        plan = self.document.addObject("Part::Feature", "PlanWireSymbol")
+        plan.Shape = Part.Wire(
+            [
+                Part.makeLine(App.Vector(0, 0, 0), App.Vector(900, 0, 0)),
+                Part.makeLine(App.Vector(900, 0, 0), App.Vector(900, 450, 0)),
+                Part.makeLine(App.Vector(900, 450, 0), App.Vector(0, 450, 0)),
+                Part.makeLine(App.Vector(0, 450, 0), App.Vector(0, 0, 0)),
+            ],
+        )
+
+        equip = Arch.makeEquipment(box)
+        equip.PlanSymbols = [plan]
+        self.document.recompute()
+
+        faces = equip.Proxy.getFootprint(equip)
+        self.assertEqual(1, len(faces))
+        self.assertAlmostEqual(900 * 450, faces[0].Area, delta=0.1)
+        self.assertAlmostEqual(equip.Shape.BoundBox.ZMin, faces[0].BoundBox.ZMin, delta=0.001)
