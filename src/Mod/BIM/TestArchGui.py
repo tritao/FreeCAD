deba38473495c041e16af57e6d293c358d2be796
Joao Matos <joao@tritao.eu>
2026-04-16 12:35:45 +0100

    BIM: add slab footprint display mode and tests

--- src/Mod/BIM/TestArchGui.py
+++ src/Mod/BIM/TestArchGui.py
@@ -31,6 +31,7 @@ from bimtests.TestArchFootprintGui import TestArchFootprintGui
 from bimtests.TestArchStairsGui import TestArchStairsGui
 from bimtests.TestArchReportGui import TestArchReportGui
 from bimtests.TestArchSiteGui import TestArchSiteGui
+from bimtests.TestArchStructureGui import TestArchStructureGui
 from bimtests.TestArchWallGui import TestArchWallGui
 from bimtests.TestArchWindowGui import TestArchWindowGui
 from bimtests.TestWebGLExportGui import TestWebGLExportGui
