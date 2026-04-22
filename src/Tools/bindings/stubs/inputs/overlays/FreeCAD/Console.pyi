7c1851f63322d769516ffc0c6e6fc6f361fc6fd7
Joao Matos <joao@tritao.eu>
2026-04-22 13:17:38 +0100

    Move module APIs into source-adjacent stubs

--- src/Tools/bindings/stubs/inputs/overlays/FreeCAD/Console.pyi
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: LGPL-2.1-or-later
-
-from __future__ import annotations
-
-from typing import Literal, TypeAlias
-
-_MessageType: TypeAlias = Literal["Log", "Wrn", "Msg", "Err", "Critical", "Notification"]
--- src/Tools/bindings/stubs/inputs/overlays/FreeCAD/Qt.pyi
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: LGPL-2.1-or-later
-
-from __future__ import annotations
-
-from typing import TypeVar
-
-_T = TypeVar("_T")
