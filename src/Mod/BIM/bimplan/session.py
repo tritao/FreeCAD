c8b567d6d5cba357ff6907935e8b989907249f0f
Joao Matos <joao@tritao.eu>
2026-04-23 13:15:42 +0100

    BIM: extract Plan Edit additive selection helpers
    Move additive selection detection, provider overlay target activation, raw object selection toggling, and target selection toggling into bimplan.selection while retaining PlanEditSession wrappers.

--- src/Mod/BIM/bimplan/session.py
+++ src/Mod/BIM/bimplan/session.py
@@ -5614,15 +5614,7 @@ class PlanEditSession:
         return plan_picking.update_hovered_plan_target(self, mouse_pos, force=force)
 
     def _is_plan_additive_selection_active(self):
-        if self.current_tool != "Select":
-            return False
-        try:
-            from PySide import QtCore, QtGui
-
-            modifiers = QtGui.QApplication.keyboardModifiers()
-            return bool(modifiers & QtCore.Qt.ControlModifier)
-        except Exception:
-            return False
+        return plan_selection.is_plan_additive_selection_active(self)
 
     def _get_plan_target_from_edit_node(self, node):
         return plan_picking.get_plan_target_from_edit_node(self, node)
@@ -5631,134 +5623,28 @@ class PlanEditSession:
         return plan_picking.get_provider_overlay_target_from_edit_node(self, node)
 
     def _activate_provider_overlay_target_node(self, node, event_callback=None):
-        target_kind, target_obj = self._get_provider_overlay_target_from_edit_node(node)
-        if target_obj is None:
-            return False
-        if self._is_valid_plan_target(target_kind, target_obj):
-            self._provider_selected_objects = []
-            self._set_pending_selected_plan_target(target_kind, target_obj)
-        else:
-            self._provider_selected_objects = [target_obj]
-            self._set_pending_selected_plan_target()
-        self._set_hovered_wall(None)
-        self._set_hovered_opening(None)
-        self._set_hovered_symbol(None)
-        self._set_hovered_provider(None)
-        self._set_hovered_space(None)
-        self._set_hovered_region(None)
-        self._set_gui_selection_object(target_obj)
-        self._refresh_primary_selected_plan_target()
-        self._claim_left_button_click(event_callback)
-        return True
-
-    def _normalize_gui_object_selection(self, selection):
-        normalized_selection = []
-        seen = set()
-        for selected in selection or ():
-            if not selected:
-                continue
-            key = (
-                getattr(getattr(selected, "Document", None), "Name", None),
-                getattr(selected, "Name", None),
-            )
-            if key in seen:
-                continue
-            seen.add(key)
-            normalized_selection.append(selected)
-        return normalized_selection
-
-    def _toggle_raw_plan_object_selection(self, obj, event_callback=None):
-        if obj is None:
-            return False
-
-        primary_kind, primary_obj = self._get_selected_plan_target()
-        selection = self._get_gui_selection()
-        if primary_obj is not None and primary_obj not in selection:
-            selection = [primary_obj] + selection
-        selection = self._normalize_gui_object_selection(selection)
-
-        provider_selection = self._normalize_gui_object_selection(self._provider_selected_objects)
-        if obj in provider_selection:
-            provider_selection = [selected for selected in provider_selection if selected != obj]
-        else:
-            provider_selection.append(obj)
-        self._provider_selected_objects = self._normalize_gui_object_selection(provider_selection)
-        new_selection = self._normalize_gui_object_selection(
-            [selected for selected in selection if self._get_plan_target_for_object(selected)[0]]
+        return plan_selection.activate_provider_overlay_target_node(
+            self,
+            node,
+            event_callback=event_callback,
         )
 
-        if primary_obj is not None and primary_obj in new_selection:
-            next_kind, next_obj = primary_kind, primary_obj
-        else:
-            next_kind, next_obj = self._get_first_plan_target_from_selection(new_selection)
+    def _normalize_gui_object_selection(self, selection):
+        return plan_selection.normalize_gui_object_selection(selection)
 
-        self._set_pending_selected_plan_target(next_kind, next_obj)
-        self._set_hovered_wall(None)
-        self._set_hovered_opening(None)
-        self._set_hovered_symbol(None)
-        self._set_hovered_provider(None)
-        self._set_hovered_space(None)
-        self._set_hovered_region(None)
-        self._set_gui_selection(new_selection)
-        self._refresh_primary_selected_plan_target()
-        self._claim_left_button_click(event_callback)
-        return True
+    def _toggle_raw_plan_object_selection(self, obj, event_callback=None):
+        return plan_selection.toggle_raw_plan_object_selection(
+            self,
+            obj,
+            event_callback=event_callback,
+        )
 
     def _toggle_plan_target_selection_at_position(self, mouse_pos, event_callback=None):
-        node = self._get_edit_node(mouse_pos)
-        if node and node[0] in ("provider_overlay_point", "provider_overlay_target"):
-            target_kind, target_obj = self._get_provider_overlay_target_from_edit_node(node)
-            if target_obj is not None and not self._is_valid_plan_target(
-                target_kind,
-                target_obj,
-            ):
-                return self._toggle_raw_plan_object_selection(target_obj, event_callback)
-        else:
-            target_kind, target_obj = self._get_plan_target_from_edit_node(node)
-        if target_kind is None:
-            target_kind, target_obj = self._get_plan_target_at_position(mouse_pos)
-        if not target_kind or not target_obj:
-            return False
-
-        primary_kind, primary_obj = self._get_selected_plan_target()
-        selection = self._get_gui_selection()
-        if primary_obj is not None and primary_obj not in selection:
-            selection = [primary_obj] + selection
-
-        selection = self._normalize_gui_object_selection(selection)
-
-        was_selected = target_obj in selection
-        if was_selected:
-            new_selection = [selected for selected in selection if selected != target_obj]
-            if primary_obj == target_obj:
-                next_kind, next_obj = self._get_first_plan_target_from_selection(new_selection)
-            elif primary_obj is not None and primary_obj in new_selection:
-                next_kind, next_obj = primary_kind, primary_obj
-            else:
-                next_kind, next_obj = self._get_first_plan_target_from_selection(new_selection)
-        else:
-            new_selection = list(selection)
-            new_selection.append(target_obj)
-            if (
-                primary_obj is not None
-                and primary_obj in new_selection
-                and primary_obj != target_obj
-            ):
-                next_kind, next_obj = primary_kind, primary_obj
-            else:
-                next_kind, next_obj = target_kind, target_obj
-
-        self._set_pending_selected_plan_target(next_kind, next_obj)
-        self._set_hovered_wall(None)
-        self._set_hovered_opening(None)
-        self._set_hovered_symbol(None)
-        self._set_hovered_provider(None)
-        self._set_hovered_space(None)
-        self._set_hovered_region(None)
-        self._set_gui_selection(new_selection)
-        self._refresh_primary_selected_plan_target()
-        self._claim_left_button_click(event_callback)
-        return True
+        return plan_selection.toggle_plan_target_selection_at_position(
+            self,
+            mouse_pos,
+            event_callback=event_callback,
+        )
 
     def _clear_hovered_plan_targets(self, kinds=None):
         return plan_picking.clear_hovered_plan_targets(self, kinds=kinds)
