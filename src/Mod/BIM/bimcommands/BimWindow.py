616cd9945868780a55d91932920d924a3dc48a8b
Joao Matos <joao@tritao.eu>
2026-09-14 08:05:55 +0100

    BIM: route the classic Window command through Opening construction

--- src/Mod/BIM/bimcommands/BimWindow.py
+++ src/Mod/BIM/bimcommands/BimWindow.py
@@ -28,6 +28,7 @@ import os
 
 import FreeCAD
 import FreeCADGui
+import ArchOpeningConstruction
 
 QT_TRANSLATE_NOOP = FreeCAD.Qt.QT_TRANSLATE_NOOP
 translate = FreeCAD.Qt.translate
@@ -106,38 +107,32 @@ class Arch_Window:
                         if obj.Objects[0].Inlist:
                             host = obj.Objects[0].Inlist[0]
 
-                    self.doc.openTransaction(translate("Arch", "Create Window"))
-                    FreeCADGui.addModule("Arch")
-                    FreeCADGui.doCommand(
-                        "win = Arch.makeWindow(FreeCAD.ActiveDocument." + obj.Name + ")"
-                    )
+                    hosts = []
                     if self.Include and host is not None and Draft.getType(host) in ALLOWEDHOSTS:
-                        FreeCADGui.doCommand(
-                            "win.Hosts = [FreeCAD.ActiveDocument." + host.Name + "]"
-                        )
+                        hosts.append(host)
                         siblings = self._get_host_siblings(host)
-                        sibs = [host]
                         for sibling in siblings:
-                            if not sibling in sibs:
-                                sibs.append(sibling)
-                                FreeCADGui.doCommand(
-                                    "win.Hosts = win.Hosts + [FreeCAD.ActiveDocument."
-                                    + sibling.Name
-                                    + "]"
-                                )
-                    self.doc.commitTransaction()
-                    self.doc.recompute()
+                            if sibling not in hosts:
+                                hosts.append(sibling)
+                    ArchOpeningConstruction.construct_opening_from_base(
+                        self.doc,
+                        obj,
+                        ArchOpeningConstruction.OpeningConstructionSpec(),
+                        transaction_name=translate("Arch", "Create Window"),
+                        hosts=hosts,
+                    )
                     return
 
                 # Try to detect an object to use as a window type - TODO we must make this safer
 
                 elif obj.Shape.Solids and (Draft.getType(obj) not in ["Wall", "Structure", "Roof"]):
                     # we consider the selected object as a type
-                    self.doc.openTransaction(translate("Arch", "Create Window"))
-                    FreeCADGui.addModule("Arch")
-                    FreeCADGui.doCommand("Arch.makeWindow(FreeCAD.ActiveDocument." + obj.Name + ")")
-                    self.doc.commitTransaction()
-                    self.doc.recompute()
+                    ArchOpeningConstruction.construct_opening_from_base(
+                        self.doc,
+                        obj,
+                        ArchOpeningConstruction.OpeningConstructionSpec(),
+                        transaction_name=translate("Arch", "Create Window"),
+                    )
                     return
 
         # interactive mode
@@ -193,12 +188,11 @@ class Arch_Window:
     def getPoint(self, point=None, obj=None):
         "this function is called by the snapper when it has a 3D point"
 
+        import __main__
         import Draft
         from draftutils import gui_utils
         from draftutils.messages import _wrn
         from ArchWindowPresets import WindowPresets
-
-        SketchArch = False
 
         self.wp._restore()
         FreeCAD.activeDraftCommand = None
@@ -210,8 +204,6 @@ class Arch_Window:
         if self.sel:
             obj = self.sel[0]
         point = point.add(FreeCAD.Vector(0, 0, self.SillHeight))
-        self.doc.openTransaction(translate("Arch", "Create Window"))
-
         FreeCADGui.doCommand("import FreeCAD, Arch, DraftGeomUtils, WorkingPlane")
         FreeCADGui.doCommand("wp = WorkingPlane.get_working_plane()")
 
@@ -237,6 +229,7 @@ class Arch_Window:
             + str(point.z)
             + ")"
         )
+        placement = FreeCAD.Placement(__main__.pl)
 
         if self.baseFace is not None:
             host = self.baseFace[0]
@@ -246,52 +239,67 @@ class Arch_Window:
             host = None
 
         if self.Preset >= len(WindowPresets):
-            preset = False
             # library object
-            col = self.doc.Objects
             path = self.librarypresets[self.Preset - len(WindowPresets)][1]
-            FreeCADGui.doCommand("FreeCADGui.ActiveDocument.mergeProject(" + repr(path) + ")")
-            # find the latest added window
-            nol = self.doc.Objects
-            for o in nol[len(col) :]:
-                if Draft.getType(o) == "Window":
-                    if Draft.getType(o.Base) != "Sketcher::SketchObject":
+
+            def import_library_opening():
+                previous_count = len(self.doc.Objects)
+                FreeCADGui.doCommand(
+                    "FreeCADGui.ActiveDocument.mergeProject(" + repr(path) + ")"
+                )
+                for opening in self.doc.Objects[previous_count:]:
+                    if Draft.getType(opening) != "Window":
+                        continue
+                    FreeCADGui.doCommand(
+                        "win = FreeCAD.ActiveDocument.getObject('" + opening.Name + "')"
+                    )
+                    if Draft.getType(opening.Base) != "Sketcher::SketchObject":
                         _wrn(
                             translate(
-                                "Arch", "Window not based on sketch. Window not aligned or resized."
+                                "Arch",
+                                "Window not based on sketch. Window not aligned or resized.",
                             )
                         )
                         self.Include = False
-                        break
-                    FreeCADGui.doCommand("win = FreeCAD.ActiveDocument.getObject('" + o.Name + "')")
+                        return opening
                     FreeCADGui.doCommand("win.Base.Placement = pl")
-                    # Historically, this normal was deduced by the orientation of the Base Sketch and hardcoded in the Normal property.
-                    # Now with the new AutoNormalReversed property/flag, set True as default, the auto Normal previously in opposite direction to is now consistent with that previously hardcoded.
-                    # With the normal set to 'auto', window object would not suffer weird shape if the Base Sketch is rotated by some reason.
-                    # Keep the property be 'auto' (0,0,0) here.
-                    # FreeCADGui.doCommand("win.Normal = pl.Rotation.multVec(FreeCAD.Vector(0, 0, -1))")
                     FreeCADGui.doCommand("win.Width = " + str(self.Width))
                     FreeCADGui.doCommand("win.Height = " + str(self.Height))
                     FreeCADGui.doCommand("win.Base.recompute()")
-                    if not self.has_width_and_height_constraint(o.Base):
+                    if not self.has_width_and_height_constraint(opening.Base):
                         _wrn(
                             translate(
                                 "Arch",
                                 "No Width and/or Height constraint in window sketch. Window not resized.",
                             )
                         )
-                    break
-            else:
-                _wrn(translate("Arch", "No window found. Cannot continue."))
+                    return opening
                 self.Include = False
+                raise ArchOpeningConstruction.OpeningConstructionError(
+                    translate("Arch", "No window found. Cannot continue.")
+                )
+
+            def host_library_opening(opening):
+                if self.Include and host is not None and Draft.getType(host) in ALLOWEDHOSTS:
+                    ArchOpeningConstruction.assign_hosts(
+                        opening, self._opening_hosts(host)
+                    )
+
+            win = ArchOpeningConstruction.construct_opening(
+                self.doc,
+                import_library_opening,
+                transaction_name=translate("Arch", "Create Window"),
+                after_hosting=host_library_opening,
+            )
 
         else:
             # preset
-            preset = True
-            wp = ""
-            for p in self.wparams:
-                wp += ", " + p.lower() + "=" + str(getattr(self, p))
             import ArchSketchObject
+
+            preset_spec = ArchOpeningConstruction.OpeningPresetSpec(
+                WindowPresets[self.Preset],
+                *(getattr(self, name) for name in self.wparams),
+            )
 
             if (
                 host
@@ -308,57 +316,56 @@ class Arch_Window:
                     # - see https://github.com/FreeCAD/FreeCAD/issues/24903#issuecomment-3475455946
                     # placement = FreeCAD.Placement(App.Vector(0,0,0),App.Rotation(App.Vector(1,0,0),90))
                     # TODO 2025.11.1 : To improve the algorithm to be more robust to allow the Base Sketch in any orientation but without problem
-                    FreeCADGui.doCommand(
-                        "pl90 = FreeCAD.Placement(App.Vector(0,0,0),App.Rotation(App.Vector(1,0,0),90))"
-                    )
-                    FreeCADGui.doCommand(
-                        "win = Arch.makeWindowPreset('"
-                        + WindowPresets[self.Preset]
-                        + "' "
-                        + wp
-                        + ", placement=pl90"
-                        + ")"
+                    preset_placement = FreeCAD.Placement(
+                        FreeCAD.Vector(),
+                        FreeCAD.Rotation(FreeCAD.Vector(1, 0, 0), 90),
                     )
                 else:
                     # Window base sketch's placement follow getPoint placement if addon exists but NOT self.Include
-                    FreeCADGui.doCommand(
-                        "win = Arch.makeWindowPreset('"
-                        + WindowPresets[self.Preset]
-                        + "' "
-                        + wp
-                        + ", placement=pl"
-                    )
-                    FreeCADGui.doCommand("win.AttachToAxisOrSketch = 'None'")
-                FreeCADGui.doCommand("FreeCADGui.Selection.addSelection(win)")
-                w = FreeCADGui.Selection.getSelection()[0]
-                FreeCADGui.doCommand("FreeCAD.SketchArchPl = pl")
-                wPl = FreeCAD.SketchArchPl
-                SketchArch = True
+                    preset_placement = placement
+                hosts = self._opening_hosts(host) if self.Include else ()
+
+                def after_hosting(opening):
+                    if not self.Include:
+                        opening.AttachToAxisOrSketch = "None"
+                    else:
+                        ArchSketchObject.attachToHost(opening, target=host, pl=placement)
+
+                win = ArchOpeningConstruction.construct_preset_opening(
+                    self.doc,
+                    preset_spec,
+                    placement=preset_placement,
+                    transaction_name=translate("Arch", "Create Window"),
+                    hosts=hosts,
+                    after_hosting=after_hosting,
+                )
+                FreeCADGui.Selection.addSelection(win)
             else:
-                FreeCADGui.doCommand(
-                    "win = Arch.makeWindowPreset('"
-                    + WindowPresets[self.Preset]
-                    + "' "
-                    + wp
-                    + ", placement = pl)"
+                win = ArchOpeningConstruction.construct_preset_opening(
+                    self.doc,
+                    preset_spec,
+                    placement=placement,
+                    transaction_name=translate("Arch", "Create Window"),
+                    hosts=self._opening_hosts(host) if self.Include else (),
                 )
-                SketchArch = False
 
-        if self.Include and host is not None and Draft.getType(host) in ALLOWEDHOSTS:
-            FreeCADGui.doCommand("win.Hosts = [FreeCAD.ActiveDocument." + host.Name + "]")
-            siblings = self._get_host_siblings(host)
-            for sibling in siblings:
-                FreeCADGui.doCommand(
-                    "win.Hosts = win.Hosts + [FreeCAD.ActiveDocument." + sibling.Name + "]"
-                )
-            if SketchArch:
-                ArchSketchObject.attachToHost(w, target=host, pl=wPl)
-
-        self.doc.commitTransaction()
         self.doc.recompute()
         # gui_utils.end_all_events()  # Causes a crash on Linux.
         self.tracker.finalize()
         return
+
+    def _opening_hosts(self, host):
+        if host is None:
+            return ()
+        import Draft
+
+        if Draft.getType(host) not in ALLOWEDHOSTS:
+            return ()
+        hosts = [host]
+        for sibling in self._get_host_siblings(host):
+            if sibling not in hosts:
+                hosts.append(sibling)
+        return tuple(hosts)
 
     def update(self, point, info):
         "this function is called by the Snapper when the mouse is moved"
